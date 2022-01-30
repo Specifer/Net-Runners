d0687d79de40330d892348dde1ee348d28f70877
Specifer <64122067+Specifer@users.noreply.github.com>
2022-01-30 13:55:02 +0530

    New Patch
    First Time doing This.

--- functions/events/discord/message/create.js
+++ functions/events/discord/message/create.js
@@ -2,26 +2,14 @@
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
 let messageContent = [
-  `Hey there! You said **${context.params.event.content}**!`,
+  `Hi! You said **${context.params.event.content}**!`,
   '',
-  `This is an example Autocode handler that responds to messages containing "hi", "hey", "hello", or "sup".`,
+  `This is Covid Helper bot here, I respond to messages containing "hi", "hey", "hello", or "sup".`,
   ''
 ];
 
-if (context.service.environment) {
-  messageContent = messageContent.concat([
-    `If you're the bot creator, you can modify this behavior by editing the Autocode project's code here:`,
-    `https://autocode.com/p/${context.service.name}/${context.service.environment}/?filename=${encodeURIComponent('functions/' + context.name + '.js')}`
-  ]);
-} else {
-  messageContent = messageContent.concat([
-    `If you're the bot creator, you can modify this behavior by editing the Autocode project's code from your dashboard:`,
-    `https://autocode.com/dashboard/`
-  ]);
-}
-
 // Only respond to messages containing the word "hi", "hey", "hello", or "sup"
-if (context.params.event.content.match(/\bhi\b|\bhey\b|\bhello\b|\bsup\b/i)) {
+if (context.params.event.content.match(/\bhi\b|\bhey\b|\bhello\b|\bsup\b|\bboo\b/i)) {
   await lib.discord.channels['@0.0.6'].messages.create({
     channel_id: context.params.event.channel_id,
     content: messageContent.join('\n'),
@@ -29,4 +17,61 @@ if (context.params.event.content.match(/\bhi\b|\bhey\b|\bhello\b|\bsup\b/i)) {
       message_id: context.params.event.id
     }
   });
-}
+} 
+
+let _messageContent = [
+  ` Hello, You are having covid related trouble, no worries I'm here to help!`,
+  '',
+  `This is Covid Helper bot here. Let's start with your symptoms `,
+  ''
+];
+
+// Only respond to messages containing the word "covid", "corona"
+if (context.params.event.content.match(/\bcovid\b|\bcorona\b|\bcough\b/i)) {
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: _messageContent.join('\n'),
+    message_reference: {
+      message_id: context.params.event.id
+    }
+  });
+} 
+
+
+let symptoms = [
+  ` Are you having any of these problems along with Fever, Cough, Runny nose **${context.params.event.content}**`,
+  '',
+  `This is Covid Helper bot here. Let's start with your symptoms `,
+  ''
+];
+
+// Only respond to messages containing the word fever,cold,headache,breathing problems
+if (context.params.event.content.match(/\bcovid\b|\bhighfever\b|\bfever\b|\bcold\b|\bcorona\b|\bcough\b/i)) {
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: symptoms.join('\n'),
+    message_reference: {
+      message_id: context.params.event.id
+    }
+  });
+} 
+
+
+let confirm = [
+  ` I think you have covid, Please visit the nearest hospital to get best treatment`,
+  '',
+  `This is Covid Helper bot here. Happy to help u.`,
+  ''
+];
+
+// Only respond to messages containing the word fever,cold,headache,breathing problems
+if (context.params.event.content.match(/\bI Have\b|\bYes\b|\bFever\b/i)) {
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: confirm.join('\n'),
+    message_reference: {
+      message_id: context.params.event.id
+    }
+  });
+} 
+
